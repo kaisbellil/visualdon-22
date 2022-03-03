e472376a09372ffa933939776c16e96c2b96e224
kaisbellil <71979258+kaisbellil@users.noreply.github.com>
2022-03-03 16:40:43 +0100

    Update script.js

---
+++ 01-SVG/js/script.js
@@ -0,0 +1,35 @@
+function domForEach(selector, callback) {
+    document.querySelectorAll(selector).forEach(callback);
+}
+
+function domOn(selector, event, callback) {
+    domForEach(selector, ele => ele.addEventListener(event, callback));
+}
+
+function isOdd(num) {
+    if (num % 2 == true) {
+    }
+    return num % 2;
+}
+
+domOn('.rectangle', 'click', evt => {
+    const monRectangle = evt.target;
+    const color = monRectangle.getAttribute('fill');
+
+    if (color == "green") {
+        monRectangle.setAttribute('fill', 'blue');
+
+    } else {
+        monRectangle.setAttribute('fill', 'green');
+
+    }
+
+})
+
+domOn('.donut', 'mouseover', evt => {
+    const monDonut = evt.target;
+    console.log(monDonut);
+    const rayon = monDonut.getAttribute('r');
+    monDonut.setAttribute('r', rayon * 1.2)
+})
+
